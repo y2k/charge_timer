2db9bc832e9efeaf5674e162ce0a20ad893092b7
y2k <itwisterlx@gmail.com>
2024-02-03 09:24:33 +0100

    generate kotlin

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-run: build build_html
+run: build build_html build_kotlin
 	cd .android && ./gradlew installDebug && adb shell am start -n im.y2k.chargetimer/.MainActivity
 
 build_html: build
 	node .android/app/src/main/assets/js/main.js > .android/app/src/main/assets/index.html
 
+build_kotlin:
+	clj2js kt src/main.android.clj > .android/app/src/main/java/im/y2k/chargetimer/GeneratedMain.kt
+
 build:
 	clj2js src/main.clj > .android/app/src/main/assets/js/main.js
 
-.PHONY: run build_html build
+.PHONY: run build_html build build_kotlin
---
+++ src/main.android.clj
@@ -0,0 +1,33 @@
+(ns im.y2k.chargetimer)
+
+(defn mainAndroid [^android.app.Activity activity ^android.webkit.WebView webView]
+  (let [webSettings (.getSettings webView)]
+    (.setJavaScriptEnabled webSettings true)
+    (.addJavascriptInterface
+     webView
+     (proxy [] []
+       android.webkit.JavascriptInterface
+       (registerBroadcast [_ ^String topic ^String action]
+         (.runOnUiThread activity
+                         (fn []
+                           (do_register_receiver
+                            activity action
+                            (fn [^String json]
+                              (.evaluateJavascript webView (str topic "('" json "')") null)))))))
+     "Android")
+    (.loadUrl webView "file:///android_asset/index.html")))
+
+(defn do_register_receiver [^android.content.Context context ^String action ^"(String)->Unit" callback]
+  (.registerReceiver
+   context
+   (proxy
+    [android.content.BroadcastReceiver] []
+     (onReceive [_ ^android.content.Context context ^android.content.Intent intent]
+       (let [extras (requireNotNull intent.extras)
+             allValues (->
+                        extras
+                        .keySet
+                        (.map (fn [key] (Pair. key (.get extras key))))
+                        (.associate (fn [x] x)))]
+         (callback (.toJson (com.google.gson.Gson.) allValues)))))
+   (android.content.IntentFilter. action)))
