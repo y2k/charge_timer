bdbf0b063b020b50965825ae57c73e7c09d575d7
y2k <itwisterlx@gmail.com>
2024-03-08 11:30:59 +0100

    refactoring

--- src/interpreter/interpreter.clj
+++ src/interpreter/interpreter.clj
@@ -1,74 +1,69 @@
-;; (__unsafe_inject_code "package prelude")
-
 (ns prelude)
 
 (__unsafe_inject_code "fun plus(a: Any?, b: Any?) = (a as Int) + (b as Int)")
 (__unsafe_inject_code "fun minus(a: Any?, b: Any?) = (a as Int) - (b as Int)")
 (__unsafe_inject_code "fun getm(x: Any?, y: String): Any? = if (x is Map<*, *>) x.get(y) else error(\"require Map\")")
-(__unsafe_inject_code "fun geta(x: Any?, y: Int): Any? = if (x is List<*>) x.get(y) else error(\"require List\")")
-(__unsafe_inject_code "fun <T> getm(x: Map<Any?, T>, y: String): T? = x[y]")
 (__unsafe_inject_code "fun <T> geta(x: List<T>, y: Int): T = x[y]")
 (__unsafe_inject_code "data class Env(val bindings: Map<String, Any?>)")
-(__unsafe_inject_code "typealias Sexp = Any")
-(__unsafe_inject_code "val Sexp.type: String get() = if (this is List<*>) \"list\" else \"atom\"")
-(__unsafe_inject_code "val Sexp.value: String get() = if (this is List<*>) { (this as List<Any>).get(0) as String } else (this as String)")
-(__unsafe_inject_code "val Sexp.args: List<Any> get() = (this as List<Any>).subList(1, size)")
 
-(defn ^Any? inter [^Env env ^Sexp node]
-  (case node.type
-    :list (case node.value
+(defn ^Any? inter [^Env env ^Any? node]
+  (if (is node List<*>)
+    (let [args (.subList node 1 node.size)]
+      (case (get node 0)
+        "hash-map" (->
+                    args
+                    (.chunked 2)
+                    (.map (fn [[k v]] (Pair. (inter env k) (inter env v))))
+                    (.toMap))
 
-            "hash-map" (->
-                        node.args
-                        (.chunked 2)
-                        (.map (fn [[k v]] (Pair. (inter env k) (inter env v))))
-                        (.toMap))
+        "vector" (.map args (fn [x] (inter env x)))
 
-            "let*" (let [[binding] node.args]
-                     (defn ^Any? loop [^Env env ^"List<Sexp>" xs]
-                       (if (= 0 xs.size)
-                         env
-                         (let [[kn vn] xs
-                               k (str kn)
-                               v (inter env vn)]
-                           (loop
-                            (__unsafe_inject_code "env.copy(bindings = env.bindings.plus(k to v))")
-                             (.drop xs 2)))))
-                     (let [env (as (loop env (as binding "List<Sexp>")) Env)
-                           body (.drop node.args 1)]
-                       (defn ^Any? loop [^"List<Sexp>" xs]
-                         (if (= 1 xs.size)
-                           (inter env (get xs 0))
-                           (do
-                             (inter env (get xs 0))
-                             (loop (.drop xs 1)))))
-                       (loop body)))
+        "let*" (let [[binding] args]
+                 (defn ^Any? loop [^Env env ^"List<Any>" xs]
+                   (if (= 0 xs.size)
+                     env
+                     (let [[kn vn] xs
+                           k (str kn)
+                           v (inter env vn)]
+                       (loop
+                        (__unsafe_inject_code "env.copy(bindings = env.bindings.plus(k to v))")
+                         (.drop xs 2)))))
+                 (let [env (as (loop env (as binding "List<Any>")) Env)
+                       body (.drop args 1)]
+                   (defn ^Any? loop [^"List<Any?>" xs]
+                     (if (= 1 xs.size)
+                       (inter env (get xs 0))
+                       (do
+                         (inter env (get xs 0))
+                         (loop (.drop xs 1)))))
+                   (loop body)))
 
-            "fn*" (let [args_node (as (get node.args 0) "List<String>")
-                        body (.subList node.args 1 node.args.size)]
-                    (fn [^"List<Any?>" args]
-                      (let [env (Env. (.plus env.bindings (.toMap (.zip args_node args))))]
-                        (.fold body (as null "Any?") (fn [_ node] (inter env node))))))
+        "fn*" (let [args_node (as (get args 0) "List<String>")
+                    body (.subList args 1 args.size)]
+                (fn [^"List<Any?>" args]
+                  (let [env (Env. (.plus env.bindings (.toMap (.zip args_node args))))]
+                    (.fold body (as null "Any?") (fn [_ node] (inter env node))))))
 
-            "if" (let [[cond then_ else_] node.args]
-                   (if (= true (inter env cond)) (inter env then_) (inter env else_)))
+        "if" (let [[cond then_ else_] args]
+               (if (= true (inter env cond)) (inter env then_) (inter env else_)))
 
-            "." (let [[source method] node.args]
-                  (let [args (-> node.args (.drop 2) (.map (fn [x] (inter env x))))
-                        i (inter env source)
-                        cls (if (not= null i)
-                              i.javaClass
-                              (Class/forName (as source String)))]
-                    (-> cls.methods
-                        (.first (fn [x] (and
-                                         (or (not= i null) (java.lang.reflect.Modifier/isStatic (.getModifiers x)))
-                                         (= x.name method))))
-                        (.invoke i (spread (.toTypedArray args))))))
+        "." (let [[source method] args]
+              (let [args (-> args (.drop 2) (.map (fn [x] (inter env x))))
+                    i (inter env source)
+                    cls (if (not= null i)
+                          i.javaClass
+                          (Class/forName (as source String)))]
+                (-> cls.methods
+                    (.first (fn [x] (and
+                                     (or (not= i null) (java.lang.reflect.Modifier/isStatic (.getModifiers x)))
+                                     (= x.name method))))
+                    (.invoke i (spread (.toTypedArray args))))))
 
-            (let [f (as (.getOrElse env.bindings node.value (fn [] (error node.value))) "(Any?) -> Any?")]
-              (f (.map node.args (fn [x] (inter env x))))))
-    (let [value node.value]
+        (let [f (as (.getOrElse env.bindings (as (get node 0) String) (fn [] (error node))) "(Any?) -> Any?")]
+          (f (.map args (fn [x] (inter env x)))))))
+
+    (let [value (as node String)]
       (cond
         (.startsWith value "\"") (.substring value 1 (- value.length 1))
         (not= null (.toIntOrNull value)) (.toInt value)
-        :else (getm env.bindings node.value)))))
+        :else (getm env.bindings value)))))
--- src/interpreter/sample.clj
+++ src/interpreter/sample.clj
@@ -1,9 +1,9 @@
-{:play_alarm
- (fn [context]
-   (let [am (.getSystemService context (.loadClass (.getClassLoader (.getClass "")) "android.media.AudioManager"))
-         sound_stream_id 5
-         max (.getStreamMaxVolume am sound_stream_id)]
-     (.setStreamVolume am sound_stream_id max 0)
-     (let [notification (.getDefaultUri android.media.RingtoneManager 4)
-           r (.getRingtone android.media.RingtoneManager context notification)]
-       (.play r))))}
+[[:play_alarm
+  (fn [context]
+    (let [am (.getSystemService context (.loadClass (.getClassLoader (.getClass "")) "android.media.AudioManager"))
+          sound_stream_id 5
+          max (.getStreamMaxVolume am sound_stream_id)]
+      (.setStreamVolume am sound_stream_id max 0)
+      (let [notification (.getDefaultUri android.media.RingtoneManager 4)
+            r (.getRingtone android.media.RingtoneManager context notification)]
+        (.play r))))]]
